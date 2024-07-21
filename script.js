8d098dd5ddebff178891575aa9a883112411b173
ELFletch04 <64966787+ELFletch04@users.noreply.github.com>
2024-07-21 12:42:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-let username='stranger'
+
+
+function displayMessage(isCorrect) {
+    const surdsFeedback = document.getElementById('surds_1_feedback');
+    if (isCorrect) {
+        surdsFeedback.style.color = 'green';
+        surdsFeedback.textContent = 'That is Correct!';
+        
+    } else {
+        surdsFeedback.style.color = 'red';
+        surdsFeedback.textContent = 'That is not quite right!';
+    }
+}
+
+// Add event listeners to the buttons
+document.getElementById('option_a').addEventListener('click', () => displayMessage(false));
+document.getElementById('option_b').addEventListener('click', () => displayMessage(true));
+document.getElementById('option_c').addEventListener('click', () => displayMessage(false));
+document.getElementById('option_d').addEventListener('click', () => displayMessage(false));
